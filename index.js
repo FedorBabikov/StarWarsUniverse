a1f15ceabcd0d4b4c795496fe985406f08a65526
Fedor Babikov <fed.babikov@protonmail.com>
2022-02-19 22:09:10 +0100

    finished basic layout

--- index.js
+++ index.js
@@ -4,30 +4,29 @@ function main() {
   const unsplashReq = `https://api.unsplash.com/search/photos/?query=library&client_id=${unsplashAccessKey}`;
   const swapiReq = "https://swapi.dev/api/people/1/";
 
-  const unsplashLink = document.getElementById("unsplash-link");
-  const swapiLink = document.getElementById("swapi-link");
+  const buttonEl = document.getElementById("button");
 
-  unsplashLink.addEventListener("click", () =>
-    apis(unsplashReq, "unsplash-container")
-  );
-  swapiLink.addEventListener("click", () => apis(swapiReq, "swapi-container"));
+  buttonEl.addEventListener("click", () => apis([unsplashReq, swapiReq]));
 }
 
-async function apis(req, cont) {
-  const response = await fetch(req);
-  const data = await response.json();
-  const div = document.getElementById(cont);
+async function apis(requests) {
+  const reqPromises = requests.map((req) => fetch(req));
+  const responses = await Promise.all(reqPromises);
 
-  if (cont.includes("unsplash")) {
-    for (const pict of data.results) {
-      const imgWrapper = document.createElement("div");
-      imgWrapper.className = "img";
-      imgWrapper.style.backgroundImage = `url('${pict.urls.full}')`;
-      div.appendChild(imgWrapper);
-    }
-  } else {
-    div.innerHTML += `${data.name}<br>`;
+  const respPromises = responses.map((resp) => resp.json());
+  const data = await Promise.all(respPromises);
+
+  const divSwapi = document.getElementById("swapi-container");
+  const divUnsplash = document.getElementById("unsplash-container");
+
+  for (const pict of data[0].results) {
+    const imgWrapper = document.createElement("div");
+    imgWrapper.className = "img";
+    imgWrapper.style.backgroundImage = `url('${pict.urls.full}')`;
+    divUnsplash.appendChild(imgWrapper);
   }
+
+  divSwapi.innerHTML += `${data[1].name}<br>`;
 }
 
 window.addEventListener("load", main);
